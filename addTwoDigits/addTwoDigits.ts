e3c2e68981d8628fc4094fc4db8f4a195bd52ee9
Anastasios Vakirtzis <anastasiosvakirtzis@gmail.com>
2020-06-24 15:40:21 +0300

    Liked my solution better.

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,12 @@
 function addTwoDigits(n: any): number {
+  //   const nums = n.toString().split("");
 
+  //   return nums.reduce((a: string, b: string) => parseInt(a) + parseInt(b));
+
+  return Math.floor((n % 10) + n / 10);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+console.log(addTwoDigits(55));
+console.log(addTwoDigits(23));
+console.log(addTwoDigits(90));
