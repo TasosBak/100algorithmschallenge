903e2634166aa1558c6e9aa8374f2b6e27e8debb
Anastasios Vakirtzis <anastasiosvakirtzis@gmail.com>
2020-06-24 16:04:06 +0300

    Nice use of filter, forEach and a ternary.

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,13 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let biggestLength = 0;
 
+  inputArray.forEach(
+    (item) =>
+      (biggestLength =
+        biggestLength > item.length ? biggestLength : item.length)
+  );
+
+  return inputArray.filter((item) => item.length === biggestLength);
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
