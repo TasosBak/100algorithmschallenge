3a4e9f11d61e255d3e4a963c15e6712702d41af2
Anastasios Vakirtzis <anastasiosvakirtzis@gmail.com>
2020-06-24 14:55:51 +0300

    Used rest operator (...) to convert unknown number of args to an array.

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,13 @@
 function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+// use rest operator to turn unknown number of args to an array
+function add2(...param1: number[]): number {
+  return param1.reduce((acc, cur) => acc + cur);
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
-
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
