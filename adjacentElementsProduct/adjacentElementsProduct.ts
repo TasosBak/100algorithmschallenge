a672583497304e5e39aa7448a8f29e1e3ced89ef
Anastasios Vakirtzis <anastasiosvakirtzis@gmail.com>
2020-06-24 15:51:45 +0300

    Like my first solution more, might be slower.

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,26 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+  let products: number[] = [];
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    products[i] = inputArray[i] * inputArray[i + 1];
+  }
+
+  return Math.max(...products);
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+function adjacentElementsProduct2(inputArray: number[]): number {
+  let products: number[] = [];
+
+  let largestProduct = 0;
+
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    products[i] = inputArray[i] * inputArray[i + 1];
+    largestProduct =
+      largestProduct > products[i] ? largestProduct : products[i];
+  }
+
+  return largestProduct;
+}
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct2([3, 6, -2, -5, 7, 3]));
